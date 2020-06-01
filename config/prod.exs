1bd82fc0db9ac1ad0ae7291d0e8b4caf08978dc3
Andrew Barr <andrew@foxtailtechnology.com>
2020-06-01 14:07:55 +1000

    update config

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,14 @@ config :blog, BlogWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: "trfMak2t2RxNga25lfKVN/y1hIO8cTIT9l7V7tZ2FSSl2bDz+L3O3FemnvrkzLjy"
+  secret_key_base: "trfMak2t2RxNga25lfKVN/y1hIO8cTIT9l7V7tZ2FSSl2bDz+L3O3FemnvrkzLjy",
+  url: [host: "ducksnutsfishing.com", port: 80],
+  check_origin: [
+    "https://ducksnutsfishing.com",
+    "http://ducksnutsfishing.com:4000",
+    "https://www.ducksnutsfishing.com",
+    "http://www.ducksnutsfishing.com:4000"
+  ]
 
 config :blog, Blog.Subscription.Mailgun,
   list: "weather@newsletter.ducksnutsfishing.com",
