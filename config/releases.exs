099cdaa42da11a90de0e40f227291e3ebdfbbee0
Andrew Barr <andrew@foxtailtechnology.com>
2020-06-01 13:57:36 +1000

    update config

--- config/releases.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-#Testing local variable
-config :blog, BlogWeb.Endpoint, secret_key_base: System.fetch_env!("PROD_SECRET_KEY")
-config :blog, Blog.Subscription.Mailgun, password: System.fetch_env!("PROD_MAIL_KEY")
