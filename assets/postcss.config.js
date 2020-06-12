b53a5d8f950c76d0981fd1f4868f0850fac3b14b
Andrew Barr <andrew@foxtail.consulting>
2020-06-12 10:27:07 +1000

    purge unused css before minify

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,21 +1,22 @@
 const { purge } = require('tailwindcss/stubs/defaultConfig.stub');
+const purgecss = require('@fullhuman/postcss-purgecss')(
+  {
+    content: [
+      "../**/*.html.eex",
+      "../**/*.html.leex",
+      "../**/views/**/*.ex",
+      "../**/live/**/*.ex",
+      "./js/**/*.js"
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+)
 
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')(
-      {
-        content: [
-          "../**/*.html.eex",
-          "../**/*.html.leex",
-          "../**/*.ex",
-          "../**/views/**/*.ex",
-          "./js/**/*.js"
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-      }
-    )
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])    
   ]
 }
