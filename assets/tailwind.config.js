9977e6788e2211f5790293292bd2a5dc3343123e
Andrew Barr <andrew@foxtail.consulting>
2020-11-17 12:01:17 +1000

    updated tailwindcss

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,20 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+      "../**/*.html.eex",
+      "../**/*.html.leex",
+      "../**/views/**/*.ex",
+      "../**/live/**/*.ex",
+      "./js/**/*.js"
+  ],
   theme: {
     extend: {
-      colors: {
-        'duck-blue': {
-          100: '#EDF2FA',
-          200: '#D1DEF3',
-          300: '#B5CAEC',
-          400: '#7EA3DD',
-          500: '#467BCF',
-          600: '#3F6FBA',
-          700: '#2A4A7C',
-          800: '#20375D',
-          900: '#15253E',
-        },
-      }
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   variants: {},
